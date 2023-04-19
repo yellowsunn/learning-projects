2fe6e75caf80749786b3398f7d47eaf9a137115f
yellowsunn <chohankook95@gmail.com>
2023-04-19 18:48:59 +0900

    Synchronized를 이용한 동시성 문제 해결하기

--- stock-concurrency/src/main/kotlin/com/yellowsunn/stockconcurrency/service/StockService.kt
+++ stock-concurrency/src/main/kotlin/com/yellowsunn/stockconcurrency/service/StockService.kt
@@ -2,7 +2,6 @@ package com.yellowsunn.stockconcurrency.service
 
 import com.yellowsunn.stockconcurrency.domain.Stock
 import com.yellowsunn.stockconcurrency.repository.StockRepository
-import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
@@ -11,11 +10,14 @@ class StockService(
     private val stockRepository: StockRepository,
 ) {
 
-    @Transactional
+    @Synchronized
+//    @Transactional
     fun decrease(id: Long, quantity: Long) {
         val stock: Stock = stockRepository.findByIdOrNull(id)
             ?: throw IllegalArgumentException("재고 정보를 찾을 수 없습니다.")
 
         stock.decrease(quantity)
+
+        stockRepository.saveAndFlush(stock)
     }
 }
